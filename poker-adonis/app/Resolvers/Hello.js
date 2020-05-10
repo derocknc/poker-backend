8e8a5784e4d68cb9e5d013da9453edcca9f566f5
Derek Stutts <dstutts@redventures.com>
2020-05-09 21:03:35 -0400

    adds socket connection

---
+++ poker-adonis/app/Resolvers/Hello.js
@@ -0,0 +1,15 @@
+module.exports = {
+  Query: {
+    hello: () => 'World'
+  }
+}
+
+// const GraphQLError = use('GraphQLError')
+
+// module.exports = {
+//   Query: {
+//     hello: function () {
+//       throw new GraphQLError('Error Message', [...])
+//     }
+//   }
+// }
---
+++ poker-adonis/start/routes.js
@@ -0,0 +1,17 @@
+const Route = use('Route')
+const GraphQLServer = use('GraphQLServer')
+
+Route.post('/', (context) => {
+  return GraphQLServer.handle(context)
+})
+
+Route.get('/graphql', (context) => {
+  return GraphQLServer.handleUI(context)
+})
+
+// or add options (example)
+Route.get("/graphiql", (context) => {
+  return GraphQLServer.handleUI(context, {
+    passHeader: `'Authorization': '${context.request.header("Authorization")}'`
+  })
+})
